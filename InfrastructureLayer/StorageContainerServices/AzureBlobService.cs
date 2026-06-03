182616caf5d4b90fd6d51a6d7f72c7d57ca8f1cb
zyalda <--list>
2026-06-03 22:20:18 +0200

    :wq

--- InfrastructureLayer/StorageContainerServices/AzureBlobService.cs
+++ InfrastructureLayer/StorageContainerServices/AzureBlobService.cs
@@ -18,7 +18,7 @@ namespace InfrastructureLayer.StorageContainerServices
         public AzureBlobService(IConfiguration configuration)
         {
             defaultAzureCredential = new DefaultAzureCredential();
-            string storageAccountName = configuration["StorageAccount:StorageName"];
+            string storageAccountName = configuration["StorageAccount:StorageName"]; //"StorageAccountName"
             containerName = configuration["StorageAccount:ContainerName"];
 
             // Construct the blob service endpoint URI
